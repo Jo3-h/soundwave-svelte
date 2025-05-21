1ce2a727503fa0be8104b04a28c2e6c9c44cf952
Jo3-h <joe.hosking1@gmail.com>
2025-05-21 19:20:56 +0930

    built skeleton of dashboard and post endpoints

--- soundwave/src/routes/+page.svelte
+++ soundwave/src/routes/+page.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    export let data: PageData;
-    console.log(data)
+	import type { PageData } from './$types';
+	export let data: PageData;
+	console.log(data);
 </script>
 
 <div>
-    {#if data.user}
-    <img class="rounded-full" src={`https://images.weserv.nl/?url=${encodeURIComponent(data.user.profilePicture ?? '')}`} alt="img"/>
-    {/if}
-    <h1>Welcome {data.user?.username ?? ""} to SvelteKit</h1>
-    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-    {#if data.user}
+	{#if data.user}
+		<img
+			class="rounded-full"
+			src={`https://images.weserv.nl/?url=${encodeURIComponent(data.user.profilePicture ?? '')}`}
+			alt="img"
+		/>
+	{/if}
+	<h1>Welcome {data.user?.username ?? ''} to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
 
-    <a href='/signout' class="h-10 w-40 flex bg-blue-300 rounded-xl shadow-md justify-center items-center font-bold text-white">Sign out</a>
-    {:else}
-    <a href="/login" class="h-10 w-40 flex bg-blue-300 rounded-xl shadow-md justify-center items-center font-bold text-white">Login</a>
-    {/if}
+	<a class="button" href="/dashboard">Dashboard</a>
+
+	<!-- Signout / Login buttons to conditionally render on state of user -->
+	{#if data.user}
+		<a href="/signout" class="button">Sign out</a>
+	{:else}
+		<a href="/login" class="button">Login</a>
+	{/if}
 </div>
---
+++ soundwave/src/routes/dashboard/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { data } = $props();
+	import UserCard from '$lib/components/UserCard.svelte';
+</script>
+
+<div>
+	<UserCard user={data.user ?? undefined} />
+	<p>I am the dashboard</p>
+</div>
